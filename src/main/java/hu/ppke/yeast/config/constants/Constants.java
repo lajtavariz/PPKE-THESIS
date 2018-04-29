ace1cc3b3f12113afbea4073bd63f48675291a22
Zoltan Lajtavari <lajtavariz@gmail.com>
2018-04-29 16:16:15 +0200

    Add constants

---
+++ src/main/java/hu/ppke/yeast/config/constants/Constants.java
@@ -0,0 +1,19 @@
+package hu.ppke.yeast.config.constants;
+
+/**
+ * Application constants.
+ */
+public final class Constants {
+
+    // Regex for acceptable logins
+    public static final String LOGIN_REGEX = "^[_'.@A-Za-z0-9-]*$";
+
+    public static final String SYSTEM_ACCOUNT = "system";
+    public static final String ANONYMOUS_USER = "anonymoususer";
+    public static final String DEFAULT_LANGUAGE = "en";
+
+    public static final WeightMethod DEFAULT_WEIGHT_METHOD = WeightMethod.TF_IDF;
+
+    private Constants() {
+    }
+}
